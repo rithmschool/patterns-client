3ecfeef2eb1b2bcdc17914b1e6ec679197ed5efb
Shriya K Nevatia <sneva831@gmail.com>
2017-06-19 19:03:32 -0700

    Creating Login screen UI
    Not all styling is working; for example - inside of the Sign In box, it needs more responsive padding at the top and bottom. The Google login button will be a different type of component once OAuth works. And probably want to ask the Tradecraft guys how they want this page to look at smaller screen widths (assuming I should just center the column instead of offsetting it on the right).

--- patterns/src/Login.js
+++ patterns/src/Login.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import GoogleLogin from 'react-google-login';
-const BASE_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:3001';
+import './Login.css';
+import {Grid, Row, Column} from 'react-cellblock';
+import logo from './images/login-logo.png';
+
+// const BASE_URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:3001';
 
 class Login extends Component {
 
@@ -10,18 +14,36 @@ class Login extends Component {
 
     render() {
         return( 
-            <div>   
-                <p>some stuff before the login button</p>
-
-                <GoogleLogin
-                    clientId="http://368001541787-s2miejtdk4nkhgp8nj0r1u94ca475fj2.apps.googleusercontent.com/"
-                    buttonText="Login"
-                    onSuccess={this.responseGoogle}
-                    onFailure={this.responseGoogle}
-                  />
-
-                <p>some stuff after the login button</p>
-            </div>
+            <Grid columnWidth={64.5} gutterWidth={24} flexible={true} className="container">
+                <Row>  
+                    <Column offset="8/16" width="7/16" className="content-box"> 
+                        <Row className="top-padding-row">&nbsp;</Row>
+                        <Row className="logo-row">
+                            <Column offset="3/7" width="1/7">
+                                <img src={logo} alt="tradecraft logo"></img> 
+                            </Column>          
+                        </Row>
+                        <Row>   
+                            <Column offset="1/7" width="5/7">
+                                <Row className="top-padding-sign-in">&nbsp;</Row>
+                                <Row className="sign-in-box">
+                                    <h1>Sign In</h1>
+                                    <GoogleLogin
+                                        clientId="http://368001541787-s2miejtdk4nkhgp8nj0r1u94ca475fj2.apps.googleusercontent.com/"
+                                        buttonText=""
+                                        onSuccess={this.responseGoogle}
+                                        onFailure={this.responseGoogle}
+                                        tag="button"
+                                        className="google-sign-in"
+                                      />
+                                      <p className="aside">Use your Tradecraft / Rithmschool email address</p>
+                                  </Row>
+                                  <Row className="bottom-padding-sign-in">&nbsp;</Row>
+                              </Column>
+                          </Row>
+                      </Column>
+                </Row>
+            </Grid>
     )};
 }
 
