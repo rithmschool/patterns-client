a6d77e491ddf470ed3f66030e7c6457b3c62e2c8
Shriya K Nevatia <sneva831@gmail.com>
2017-06-28 17:06:13 -0700

    Right sidebar created as SidebarRight component with CSS styling

--- src/core/SidebarRight.js
+++ src/core/SidebarRight.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './SidebarRight.css';
 import bookmark from '../images/icon-diary-gray.svg';
+import lock from '../images/icon-lock-gray.svg';
+import more from '../images/icon-more-gray.svg';
+import hide from '../images/icon-open-collapse-left-gray.svg';
 
 class SidebarRight extends Component {
 
@@ -8,17 +11,19 @@ render() {
   return(
     <div className="sidebar-right">
       <div className="activity-header">
-          <div className="icon-picture"><img src={bookmark} alt="Bookmark Icon"/></div>
+          <div className="icon-bookmark"><img src={bookmark} alt="Bookmark Icon"/></div>
           <h1>Job Search (June 2016)</h1>
         </div>
 
         <div className="notes-tasks-log">
-          <div className="dropdown">
-            <h2>Research</h2>
-          </div>
+          <select name="stage" id="stage">
+            <option value="Research">Research</option>
+            <option value="Follow Up">Follow Up</option>
+            <option value="Apply">Apply</option>
+          </select>
 
           <div className="selectors">
-            <div className="select-item">
+            <div className="select-item" id="selected">
               <div className="number"><h2>2</h2></div>
               <div className="type"><h4>Notes</h4></div>
             </div>
@@ -34,10 +39,19 @@ render() {
         </div>
 
         <div className="post-box">
-          <div className="input-field"></div>
+          <div className="input-field">
+            { /* how do we change the width dynamically?
+            <textarea name="note" rows="6" cols="10">
+              Add a note
+            </textarea> */}
+          </div>
           <div className="post-info">
-            <div className="lock-icon"></div>
-            <div className="private-note"><h5>Private Note</h5></div>
+            <div className="lock-icon">
+              <img src={lock} alt="Lock Icon"/>
+            </div>
+            <div className="private-note">
+              <h5>Private<br /> Note</h5>
+            </div>
             <div className="post-button">
               <button>
                 <h3>Post</h3>
@@ -48,17 +62,28 @@ render() {
 
         <div className="notes-previews">
           <div className="indiv-note">
-            <div className="note-date"><h6>7/14/2017</h6></div>
-            <div className="note-content"><p>Lorem Ipsum</p></div>
+            <div className="note-date">
+              <h6>7/14/2017</h6>
+              <img src={more} alt="More Icon"/>
+            </div>
+            <div className="note-content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget urna magna. Aliquam sit amet erat non nunc pharetra facilisis. Nullam dignissim vestibulum tellus ut mattis. Nullam at diam tellus. Maecenas semper massa felis, vel accumsan massa pulvinar eu. Aenean et mi pretium, imperdiet nulla sed, euismod ex. Morbi leo orci, tempus ut lacus et, aliquam aliquam nibh. Ut tortor nisl, vulputate quis placerat quis, dapibus in massa. Nunc mi dolor, maximus quis magna eget, finibus consequat orci.</p>
+            </div>
           </div>
           <div className="indiv-note">
-
+            <div className="note-date">
+              <h6>7/14/2017</h6>
+              <img src={more} alt="More Icon"/>
+            </div>
+            <div className="note-content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget urna magna. Aliquam sit amet erat non nunc pharetra facilisis. Nullam dignissim vestibulum tellus ut mattis.</p>
+            </div>
           </div>
         </div>
 
-        <div className="right-footer">
+        <div className="left-footer">
           <div className="hide-holder">
-            <img src="" alt="Hide sidebar" />
+            <img src={hide} alt="Hide sidebar" />
             <h5>HIDE</h5>
           </div>
         </div>
