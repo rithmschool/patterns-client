515a67541f5f51f86d099ad41c3a1dff67dcbd96
artukam <ksk.arthur@gmail.com>
2017-07-06 11:18:06 -0700

    Fix bug relating to display/route for BrowserContainer relating to Content component

--- src/core/Content.js
+++ src/core/Content.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import './Content.css';
 import InnerContent from './InnerContent';
 import TopNav from './TopNav';
 import Header from './Header';
-import { toggleModal } from '../actions/auth';
 
 const Content = props => (
   <div className='content-holder'> 
     <TopNav />
-    <Header toggleModal={props.toggleModal} />
+    <Header />
     <InnerContent />
   </div>
 );
 
-export default connect(undefined, { toggleModal })(Content);
+export default Content;
