4024b408b476ac7cef7149fec7375961caffccb9
Sarah Farnsworth-kumli <sarah@stormpath.com>
2017-10-12 13:25:39 -0700

    removed activitywrapper css

---
+++ src/components/organisms/ActivityWrapper.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import BrowseCompanies from '../molecules/BrowseCompanies';
+import StageContainer from '../../containers/StageContainer';
+import styled from 'styled-components';
+import StyleVariables from '../../StyleVariables';
+
+const ActivityWrapperStyle = styled.div`
+  background-color: ${StyleVariables['content-background']};
+
+  .row {
+    margin: 0;
+  }
+
+  .dragTarget {
+    height: 200px;
+  }
+
+  .isDragging {
+    font-size: 15;
+    font-weight: bold;
+    cursor: move;
+  }
+`;
+
+const ActivityWrapper = props => {
+  return (
+    <ActivityWrapperStyle classname="row">
+      <BrowseCompanies />
+      <StageContainer />
+    </ActivityWrapperStyle>
+  );
+};
+
+export default ActivityWrapper;
