57583d08456bddedc7d3f0d6fed3476d4e8a3d4a
toChaim <toChaim@gmail.com>
2017-10-13 16:16:13 -0700

    fix for merge.

--- src/containers/ModalActivityContainer.js
+++ src/containers/ModalActivityContainer.js
@@ -96,7 +96,6 @@ class ModalActivityContainer extends Component {
           submitted: true
         });
         this.props.fetchActivitiesRequest(this.props.userId);
-        //this.props.toggleModal();
       });
     }
   }
