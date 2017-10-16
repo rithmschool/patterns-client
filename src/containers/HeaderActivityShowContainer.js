2357db82d967a59b9fb39ad24d6ac955ba5b0626
Sarah Farnsworth-kumli <sarah@stormpath.com>
2017-10-16 14:52:23 -0700

    changed ModalActivityContainer to ModalCompanyContainer

--- src/containers/HeaderActivityShowContainer.js
+++ src/containers/HeaderActivityShowContainer.js
@@ -4,6 +4,61 @@ import PropTypes from 'prop-types';
 import './HeaderActivityShowContainer.css';
 import { setActiveActivity } from '../store/actions/actionCreators';
 import ModalActivityContainer from './ModalActivityContainer';
+import styled from 'styled-components';
+import ModalCompanyContainer from './ModalCompanyContainer';
+
+const HeaderStyle = styled.div`
+  width: 100%;
+  background-color: #f8f9fa;
+  box-shadow: 0 3px 8px 0 rgba(0, 0, 0, 0.04), 0 1px 2px 0 rgba(0, 0, 0, 0.04);
+`;
+
+const CompanyLogoStyle = styled.div`
+  // width: 48px;
+  // height: 48px;
+  // background-color: #e7e8ec;
+  // margin: 24px auto 24px 24px;
+  // display: inline-block;
+  // float: left;
+  // background-size: contain;
+  // background-repeat: no-repeat;
+  // background-position-y: center;
+`;
+
+const HeaderTitleStyle = styled.p`
+  // font-weight: 600;
+  // float: left;
+  // vertical-align: middle;
+  // line-height: 45px;
+  // text-align: left;
+  // margin-left: 20px;
+`;
+
+const StatusToggleStyle = styled.div`
+  // border-radius: 2px;
+  // background-color: #38d682;
+  // float: left;
+  // font-weight: 600;
+  // text-align: center;
+  // color: white;
+  // text-transform: uppercase;
+`;
+
+const EditActivityButtonStyle = styled.input`
+  // background-color: #585b60;
+  // text-align: center;
+  // color: white;
+  // float: right;
+  // cursor: pointer;
+`;
+
+const AddActivityButtonStyle = styled.input`
+  // background-color: #cb9c59;
+  // text-align: center;
+  // color: white;
+  // float: right;
+  // cursor: pointer;
+`;
 
 class HeaderActivityShowContainer extends Component {
   constructor(props) {
@@ -29,7 +84,7 @@ class HeaderActivityShowContainer extends Component {
     let modal = null;
     if (this.state.modalOpen) {
       modal = (
-        <ModalActivityContainer
+        <ModalCompanyContainer
           toggleModal={this.toggleModal}
           name="Edit"
           logo="Replace"
