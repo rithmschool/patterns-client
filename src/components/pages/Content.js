f11cb6afe72c496beed43154e04341b0ba3d35e9
Sarah Farnsworth-kumli <sarah@stormpath.com>
2017-10-20 10:22:51 -0700

    added some left over styling to content.js didn't want to lose it

--- src/components/pages/Content.js
+++ src/components/pages/Content.js
@@ -3,6 +3,22 @@ import InnerContent from '../organisms/InnerContent';
 import TopNav from '../atoms/TopNav';
 import Header from '../organisms/Header';
 import ContentHolderStyle from '../atoms/ContentHolderStyle';
+import styled from 'styled-components';
+
+const NavbarStyle = styled.div`
+  margin: 0;
+  padding: 0;
+  background-color: white;
+`;
+
+const ContentNavbarStyle = styled.h2`
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 1.2;
+  color: #a0a1a5;
+  margin: 0;
+  text-align: left;
+`;
 
 const Content = props => (
   <ContentHolderStyle>
